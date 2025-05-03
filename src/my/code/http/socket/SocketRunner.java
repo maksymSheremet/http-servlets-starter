9c06cf11b634618b04466ff67d5ff68b7f4ad928
Maksym <MiToSSH@gmail.com>
2025-05-03 17:14:14 +0300

    9 - TCP Класс ServerSocket

--- src/my/code/http/socket/SocketRunner.java
+++ src/my/code/http/socket/SocketRunner.java
@@ -5,19 +5,24 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
     public static void main(String[] args) throws IOException {
         // http - 80
         // https - 443
-        var inetAddress = Inet4Address.getByName("google.com");
-        try (var socket = new Socket(inetAddress, 80);
+        var inetAddress = Inet4Address.getByName("localhost");
+        try (var socket = new Socket(inetAddress, 7777);
              var outputStream = new DataOutputStream(socket.getOutputStream());
-             var inputStream = new DataInputStream(socket.getInputStream())) {
-
-            outputStream.writeUTF("Hello world!");
-            var response = inputStream.readAllBytes();
-            System.out.println(response.length);
+             var inputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                var request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
+//            outputStream.writeUTF("Hello world!");
+//            System.out.println("Response from server: " + inputStream.readUTF());
         }
     }
 }
