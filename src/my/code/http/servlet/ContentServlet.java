c7982d456ab117bf46c22a4ba968f7724512e5d8
Maksym <MiToSSH@gmail.com>
2025-08-09 17:03:46 +0300

    42 Expression Language

--- src/my/code/http/servlet/ContentServlet.java
+++ src/my/code/http/servlet/ContentServlet.java
@@ -6,14 +6,25 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import my.code.http.util.JspHelper;
+import my.code.practice_one.dto.FlightDto;
+import my.code.practice_one.service.FlightService;
 
 import java.io.IOException;
+import java.util.List;
+
+import static java.util.stream.Collectors.toMap;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDto> flightDtos = flightService.findAll();
+        req.setAttribute("flights", flightDtos);
+        req.getSession().setAttribute("flightsMap", flightDtos.stream()
+                .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
         req.getRequestDispatcher(JspHelper.getPath("content"))
                 .forward(req,resp);
     }
