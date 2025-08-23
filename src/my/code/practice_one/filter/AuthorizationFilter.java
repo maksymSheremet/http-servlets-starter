509fbef2910561333abb5ca83dde5963aa264470
Maksym <MiToSSH@gmail.com>
2025-08-23 19:08:28 +0300

    53 Authorization

---
+++ src/my/code/practice_one/filter/AuthorizationFilter.java
@@ -0,0 +1,43 @@
+package my.code.practice_one.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import my.code.practice_one.dto.ResponseUserDto;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static my.code.practice_one.util.UrlPath.IMAGES;
+import static my.code.practice_one.util.UrlPath.LOGIN;
+import static my.code.practice_one.util.UrlPath.REGISTRATION;
+
+@WebFilter("/*")
+public class AuthorizationFilter implements Filter {
+    private static final Set<String> PUBLIC_PATH = Set.of(LOGIN, REGISTRATION, IMAGES);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var uri = ((HttpServletRequest) servletRequest).getRequestURI();
+        if (isPublicPath(uri) || isUserLoggedIn(servletRequest)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            var prevPage = ((HttpServletRequest) servletRequest).getHeader("referer");
+            ((HttpServletResponse) servletResponse).sendRedirect(prevPage != null ? prevPage : LOGIN);
+        }
+    }
+
+    private boolean isUserLoggedIn(ServletRequest servletRequest) {
+        var user = (ResponseUserDto) ((HttpServletRequest) servletRequest).getSession().getAttribute("user");
+        return user != null;
+    }
+
+    private boolean isPublicPath(String uri) {
+        return PUBLIC_PATH.stream().anyMatch(uri::startsWith);
+    }
+}
