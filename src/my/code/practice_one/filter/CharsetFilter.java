c8762608c9a6deffc2713cf808837d0f41b8c771
Maksym <MiToSSH@gmail.com>
2025-08-23 16:39:51 +0300

    50 - 49 Servlet Filters

---
+++ src/my/code/practice_one/filter/CharsetFilter.java
@@ -0,0 +1,36 @@
+package my.code.practice_one.filter;
+
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebFilter(value = "/*",
+//        servletNames = {"RegistrationServlet"},
+        initParams = {
+                @WebInitParam(name = "param1", value = "paramValue")
+        },
+        dispatcherTypes = DispatcherType.REQUEST
+)
+public class CharsetFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        servletResponse.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+}
