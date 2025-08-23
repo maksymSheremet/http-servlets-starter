1eda280de90f472e341331e08f18030b33f472df
Maksym <MiToSSH@gmail.com>
2025-08-23 15:43:47 +0300

    47 File upload Multipart form data

--- src/my/code/practice_one/mapper/CreaterUserMapper.java
+++ src/my/code/practice_one/mapper/CreaterUserMapper.java
@@ -7,6 +7,8 @@ import my.code.practice_one.entity.User;
 import my.code.practice_one.util.LocalDateFormater;
 
 public class CreaterUserMapper implements Mapper<CreateUserDto, User> {
+    private static final String IMAGE_FOLDER = "users/";
+
     private static final CreaterUserMapper INSTANCE = new CreaterUserMapper();
 
     public static CreaterUserMapper getInstance() {
@@ -19,6 +21,7 @@ public class CreaterUserMapper implements Mapper<CreateUserDto, User> {
                 .name(object.getName())
                 .birthday(LocalDateFormater.format(object.getBirthday()))
                 .email(object.getEmail())
+                .image(IMAGE_FOLDER + object.getImage().getSubmittedFileName())
                 .password(object.getPassword())
                 .gender(Gender.valueOf(object.getGender()))
                 .role(Role.valueOf(object.getRole()))
--- src/my/code/practice_one/servlet/RegistrationServlet.java
+++ src/my/code/practice_one/servlet/RegistrationServlet.java
@@ -1,10 +1,12 @@
 package my.code.practice_one.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import my.code.http.util.JspHelper;
 import my.code.practice_one.dto.CreateUserDto;
 import my.code.practice_one.exception.ValidationException;
@@ -13,6 +15,7 @@ import my.code.practice_one.service.UserService;
 import java.io.IOException;
 import java.util.List;
 
+@MultipartConfig(fileSizeThreshold = 1024 * 1024)
 @WebServlet("/registration")
 public class RegistrationServlet extends HttpServlet {
     private final UserService userService = UserService.getInstance();
@@ -31,6 +34,7 @@ public class RegistrationServlet extends HttpServlet {
                 .name(req.getParameter("name"))
                 .birthday(req.getParameter("birthday"))
                 .email(req.getParameter("email"))
+                .image(req.getPart("image"))
                 .password(req.getParameter("password"))
                 .role(req.getParameter("role"))
                 .gender(req.getParameter("gender"))
